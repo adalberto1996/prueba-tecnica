9250b4a954f563131e4341520a5eeae3a4c999c6
adalbero pedroza <yazmin57419046@gmail.com>
2021-10-21 13:22:47 -0500

    Add files via upload

---
+++ punto 1a.py
@@ -0,0 +1,24 @@
+﻿from array import array
+ 
+
+v=[8,6,3,8,2,4,5,2,2,15,10]
+
+sum=0
+
+tamano= len(v)
+
+for i in range(0,tamano):
+
+    sum=sum +v[i]
+
+media=round (sum/tamano)
+
+aux=[]
+
+for i in range(0,tamano):
+  
+  if(v[i]<=media):
+        
+aux.append(v[i])
+
+print(aux)
---
+++ ´punto-1c.py
@@ -0,0 +1,39 @@
+﻿from array import array
+
+
+v=[8,6,3,8,2,4,5,2,2,15,10]
+
+
+tamano= len(v)
+
+primo=[]
+
+bandera=False
+
+def es_primo(num):
+
+    for n in range(2, num):
+
+        if num % n == 0:
+
+            return False
+
+    return True
+
+for i in range(0,tamano):
+
+   if(es_primo(v[i]) == True):
+
+       primo.append(v[i])
+
+sum=0
+for j in range(0, len(primo)):
+
+     sum=sum +primo[j]
+
+primos = list(dict.fromkeys(primo))
+
+media=round (sum/len(primos))
+
+  
+print(media)
