ce871e79ed7bb610f626ba65464d638fb31eabd1
adalbero pedroza <yazmin57419046@gmail.com>
2021-10-25 08:28:55 -0500

    Update and rename vector_menor_media.py to moda.py

---
+++ moda.py
@@ -0,0 +1,19 @@
+﻿from array import array
+
+v=[8,6,3,8,2,4,5,2,2,15,10]
+
+
+
+tamano= len(v)
+cmoda=0
+moda=0
+for i in range(0,tamano):
+    aux=1
+    for j in range(i, tamano):
+        if(v[i]==v[j]):
+            aux=aux+1
+    if(cmoda<aux):
+        moda=v[i]
+        cmoda=aux
+
+print(moda)
